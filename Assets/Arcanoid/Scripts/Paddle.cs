33bf422e8e5c394b204ec3d152a8dc6a013b59c9
Denis Pak <agentndschool@gmail.com>
2024-09-06 21:18:30 +0300

    Implement mouse input for paddle

--- Assets/Arcanoid/Scripts/Paddle.cs
+++ Assets/Arcanoid/Scripts/Paddle.cs
@@ -5,8 +5,14 @@ public class Paddle : MonoBehaviour
     [SerializeField]
     private float speed = 1.0f;
 
+    [SerializeField] 
+    private MouseInput mouseInput;
+
     private Vector2 velocity;
     private Vector2 input;
+
+    private float xMin = -4;
+    private float xMax = 4;
 
     private void Move() { 
         transform.Translate(velocity * speed * Time.deltaTime);
@@ -16,11 +22,26 @@ public class Paddle : MonoBehaviour
     {
         GetInput();
         Move();
+
+        if (transform.position.x > xMax)
+        {
+            transform.position = new(xMax, transform.position.y, 0);
+        } 
+        else if (transform.position.x < xMin)
+        {
+            transform.position = new(xMin, transform.position.y, 0);
+        }
     }
 
     private void GetInput()
     {
-        velocity = GetMouseInput();
+        // velocity = mouseInput.MouseDelta;
+        if (mouseInput.MouseDelta != Vector2.zero)
+        {
+            transform.position = new (mouseInput.CurrentPosition.x, transform.position.y, 0);
+            return;
+        }
+
         if (velocity == Vector2.zero)
         {
             velocity = GetKeyboardInput();
@@ -37,10 +58,5 @@ public class Paddle : MonoBehaviour
         }
 
         return Vector2.zero;
-    }
-
-    private Vector2 GetMouseInput()
-    {
-        return Input.GetAxis("Mouse X") * Vector2.right * 5f;
     }
 }
